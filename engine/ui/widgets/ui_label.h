f248a656aee6f73840255362967c83e2c2540f68
Akil0814 <akil040814@gmail.com>
2026-06-21 13:57:00 -0700

    6/21

--- engine/ui/widgets/ui_label.h
+++ engine/ui/widgets/ui_label.h
@@ -1,4 +1,19 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "../core/ui_element.h"
+
+
 namespace elysia::ui
 {
+	class UiLabel : public UiElement
+	{
+		UiLabel(elysia::core::Vector2 position,elysia::core::Vector2 size,std::string text_key);
+		~UiLabel();
+
+	};
+
 
 }
