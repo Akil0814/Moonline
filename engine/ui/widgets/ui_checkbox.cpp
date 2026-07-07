e3100c8cbd22768e15b3d41f0ef048868903f98d
Akil0814 <akil040814@gmail.com>
2026-07-06 17:26:46 -0700

    7/6

---
+++ engine/ui/widgets/ui_checkbox.cpp
@@ -0,0 +1,558 @@
+#include "ui_checkbox.h"
+
+#include "../../audio/audio_service.h"
+#include "../../core/render/render_command.h"
+
+#include <algorithm>
+#include <utility>
+
+namespace elysia::ui
+{
+namespace
+{
+[[nodiscard]] bool has_visual_state_textures(const UiCheckboxVisualStateTextures& textures) noexcept
+{
+    return textures.idle
+        && textures.focused
+        && textures.pushed
+        && textures.disabled;
+}
+}
+
+UiCheckbox::UiCheckbox(const elysia::core::Rect& rect,int order) noexcept
+    : UiControl(rect,order) {}
+
+UiCheckbox::UiCheckbox(const elysia::core::Vector2& position,const elysia::core::Vector2& size,int order) noexcept
+    : UiCheckbox(elysia::core::Rect(position.x,position.y,size.x,size.y),order) {}
+
+UiCheckbox::UiCheckbox(const elysia::core::Vector2& center,const elysia::core::Vector2& size,UiFromCenterTag,int order) noexcept
+    : UiCheckbox(elysia::core::Rect::from_center(center,size),order) {}
+
+UiCheckbox::UiCheckbox(const elysia::core::Rect& rect,const UiCheckboxConfig& config,int order) noexcept
+    : UiCheckbox(rect,order)
+{
+    set_checkbox_config(config);
+}
+
+UiCheckbox::UiCheckbox(const elysia::core::Vector2& position,const elysia::core::Vector2& size,const UiCheckboxConfig& config,int order) noexcept
+    : UiCheckbox(elysia::core::Rect(position.x,position.y,size.x,size.y),config,order) {}
+
+UiCheckbox::UiCheckbox(
+    const elysia::core::Vector2& center,const elysia::core::Vector2& size,
+    UiFromCenterTag,const UiCheckboxConfig& config,int order
+) noexcept : UiCheckbox(elysia::core::Rect::from_center(center,size),config,order) {}
+
+void UiCheckbox::reset() noexcept
+{
+    UiControl::reset();
+    set_use_theme(false);
+
+    _textures.reset();
+    _sounds.reset();
+    _on_toggled = nullptr;
+    _state = UiCheckboxState::Unchecked;
+    _idle_color = elysia::core::colors::cobalt_blue;
+    _focused_color = elysia::core::colors::royal_blue;
+    _pushed_color = elysia::core::colors::midnight_blue;
+    _disabled_background_color = elysia::core::colors::gray_700;
+    _border_color = elysia::core::colors::sky_blue;
+    _disabled_border_color = elysia::core::colors::gray_500;
+    _checkmark_color = elysia::core::colors::glacial_white;
+    _disabled_checkmark_color = elysia::core::colors::gray_300;
+    _padding = 4;
+    _draw_background = true;
+    _draw_border = true;
+    _is_pushed = false;
+}
+
+void UiCheckbox::set_enabled(bool enabled)
+{
+    UiControl::set_enabled(enabled);
+    if (!enabled)
+        clear_pushed_state();
+}
+
+void UiCheckbox::set_focused(bool focused)
+{
+    const bool was_focused = is_focused();
+    UiControl::set_focused(focused);
+    if (!is_focused())
+        clear_pushed_state();
+    if (!was_focused && is_focused() && _sounds)
+        play_sound_if_set(_sounds->focus);
+}
+
+bool UiCheckbox::on_ui_input_event(const UiInputEvent& event)
+{
+    if (event.type == UiInputEventType::MouseMoved)
+    {
+        if (!can_receive_pointer())
+        {
+            set_focused(false);
+            clear_pushed_state();
+            return false;
+        }
+
+        set_focused(contains_pointer(event.mouse_x,event.mouse_y));
+        return false;
+    }
+
+    if (event.type == UiInputEventType::PointerPressed)
+    {
+        if (!is_primary_pointer_event(event) || !can_receive_pointer())
+            return false;
+        if (!contains_pointer(event.mouse_x,event.mouse_y))
+            return false;
+
+        set_focused(true);
+        _is_pushed = true;
+        if (_sounds)
+            play_sound_if_set(_sounds->press);
+        return true;
+    }
+
+    if (event.type == UiInputEventType::PointerReleased)
+    {
+        if (!is_primary_pointer_event(event))
+            return false;
+
+        const bool was_pushed = _is_pushed;
+        const bool is_inside = can_receive_pointer() && contains_pointer(event.mouse_x,event.mouse_y);
+        set_focused(is_inside);
+        clear_pushed_state();
+
+        if (was_pushed && is_inside)
+            return toggle_internal(true,true);
+
+        return was_pushed;
+    }
+
+    if (event.action != UiAction::Confirm)
+        return false;
+
+    if (!can_interact())
+    {
+        clear_pushed_state();
+        return false;
+    }
+
+    if (event.type == UiInputEventType::ActionPressed)
+    {
+        _is_pushed = true;
+        if (_sounds)
+            play_sound_if_set(_sounds->press);
+        return true;
+    }
+
+    if (event.type == UiInputEventType::ActionReleased)
+    {
+        const bool should_toggle = _is_pushed;
+        clear_pushed_state();
+        if (should_toggle)
+            return toggle_internal(true,true);
+        return false;
+    }
+
+    return false;
+}
+
+void UiCheckbox::submit_ui_render_commands(std::vector<elysia::core::UiRenderCommand>& out_commands) const
+{
+    if (!is_visible())
+        return;
+
+    const elysia::core::Rect rect = checkbox_rect();
+    if (rect.is_empty())
+        return;
+
+    if (uses_texture_rendering())
+    {
+        SDL_Texture* texture = current_state_texture();
+        if (!texture)
+            return;
+
+        elysia::core::UiRenderCommand command = elysia::core::make_ui_texture_command(texture,rect);
+        apply_opacity(command);
+        out_commands.push_back(command);
+        return;
+    }
+
+    if (_draw_background)
+        out_commands.push_back(elysia::core::make_ui_fill_rect_command(rect,apply_opacity(current_background_color())));
+    if (_draw_border)
+        out_commands.push_back(elysia::core::make_ui_draw_rect_command(rect,apply_opacity(current_border_color())));
+
+    const elysia::core::Color mark_color = apply_opacity(current_checkmark_color());
+    if (_state == UiCheckboxState::Checked)
+    {
+        const elysia::core::Vector2 start(rect.x() + rect.width() * 0.22f,rect.y() + rect.height() * 0.54f);
+        const elysia::core::Vector2 mid(rect.x() + rect.width() * 0.43f,rect.y() + rect.height() * 0.76f);
+        const elysia::core::Vector2 end(rect.x() + rect.width() * 0.80f,rect.y() + rect.height() * 0.28f);
+        out_commands.push_back(elysia::core::make_ui_draw_line_command(start,mid,mark_color));
+        out_commands.push_back(elysia::core::make_ui_draw_line_command(mid,end,mark_color));
+    }
+    else if (_state == UiCheckboxState::Indeterminate)
+    {
+        const elysia::core::Vector2 start(rect.x() + rect.width() * 0.22f,rect.center().y);
+        const elysia::core::Vector2 end(rect.x() + rect.width() * 0.78f,rect.center().y);
+        out_commands.push_back(elysia::core::make_ui_draw_line_command(start,end,mark_color));
+    }
+}
+
+void UiCheckbox::set_checkbox_config(const UiCheckboxConfig& config)
+{
+    apply_checkbox_config(config);
+}
+
+void UiCheckbox::set_state(UiCheckboxState state) noexcept
+{
+    (void)set_state_internal(state,false);
+}
+
+UiCheckboxState UiCheckbox::state() const noexcept
+{
+    return _state;
+}
+
+void UiCheckbox::set_checked(bool checked) noexcept
+{
+    (void)set_state_internal(checked ? UiCheckboxState::Checked : UiCheckboxState::Unchecked,false);
+}
+
+bool UiCheckbox::is_checked() const noexcept
+{
+    return _state == UiCheckboxState::Checked;
+}
+
+bool UiCheckbox::is_indeterminate() const noexcept
+{
+    return _state == UiCheckboxState::Indeterminate;
+}
+
+void UiCheckbox::toggle()
+{
+    (void)toggle_internal(true,false);
+}
+
+void UiCheckbox::set_state_textures(const UiCheckboxTextures& textures)
+{
+    _textures = textures;
+}
+
+void UiCheckbox::clear_state_textures() noexcept
+{
+    _textures.reset();
+}
+
+const std::optional<UiCheckboxTextures>& UiCheckbox::state_textures() const noexcept
+{
+    return _textures;
+}
+
+bool UiCheckbox::has_complete_state_textures() const noexcept
+{
+    return _textures
+        && has_visual_state_textures(_textures->unchecked)
+        && has_visual_state_textures(_textures->checked)
+        && has_visual_state_textures(_textures->indeterminate);
+}
+
+void UiCheckbox::set_sounds(const UiCheckboxSounds& sounds)
+{
+    _sounds = sounds;
+}
+
+void UiCheckbox::clear_sounds() noexcept
+{
+    _sounds.reset();
+}
+
+const std::optional<UiCheckboxSounds>& UiCheckbox::sounds() const noexcept
+{
+    return _sounds;
+}
+
+void UiCheckbox::set_on_toggled(UiCheckboxToggledCallback on_toggled)
+{
+    _on_toggled = std::move(on_toggled);
+}
+
+void UiCheckbox::set_idle_color(elysia::core::Color color) noexcept
+{
+    _idle_color = color;
+}
+
+elysia::core::Color UiCheckbox::idle_color() const noexcept
+{
+    return _idle_color;
+}
+
+void UiCheckbox::set_focused_color(elysia::core::Color color) noexcept
+{
+    _focused_color = color;
+}
+
+elysia::core::Color UiCheckbox::focused_color() const noexcept
+{
+    return _focused_color;
+}
+
+void UiCheckbox::set_pushed_color(elysia::core::Color color) noexcept
+{
+    _pushed_color = color;
+}
+
+elysia::core::Color UiCheckbox::pushed_color() const noexcept
+{
+    return _pushed_color;
+}
+
+void UiCheckbox::set_disabled_background_color(elysia::core::Color color) noexcept
+{
+    _disabled_background_color = color;
+}
+
+elysia::core::Color UiCheckbox::disabled_background_color() const noexcept
+{
+    return _disabled_background_color;
+}
+
+void UiCheckbox::set_border_color(elysia::core::Color color) noexcept
+{
+    _border_color = color;
+}
+
+elysia::core::Color UiCheckbox::border_color() const noexcept
+{
+    return _border_color;
+}
+
+void UiCheckbox::set_disabled_border_color(elysia::core::Color color) noexcept
+{
+    _disabled_border_color = color;
+}
+
+elysia::core::Color UiCheckbox::disabled_border_color() const noexcept
+{
+    return _disabled_border_color;
+}
+
+void UiCheckbox::set_checkmark_color(elysia::core::Color color) noexcept
+{
+    _checkmark_color = color;
+}
+
+elysia::core::Color UiCheckbox::checkmark_color() const noexcept
+{
+    return _checkmark_color;
+}
+
+void UiCheckbox::set_disabled_checkmark_color(elysia::core::Color color) noexcept
+{
+    _disabled_checkmark_color = color;
+}
+
+elysia::core::Color UiCheckbox::disabled_checkmark_color() const noexcept
+{
+    return _disabled_checkmark_color;
+}
+
+void UiCheckbox::set_padding(int padding) noexcept
+{
+    _padding = std::max(0,padding);
+}
+
+int UiCheckbox::padding() const noexcept
+{
+    return _padding;
+}
+
+void UiCheckbox::set_draw_background(bool draw_background) noexcept
+{
+    _draw_background = draw_background;
+}
+
+bool UiCheckbox::draws_background() const noexcept
+{
+    return _draw_background;
+}
+
+void UiCheckbox::set_draw_border(bool draw_border) noexcept
+{
+    _draw_border = draw_border;
+}
+
+bool UiCheckbox::draws_border() const noexcept
+{
+    return _draw_border;
+}
+
+void UiCheckbox::apply_checkbox_config(const UiCheckboxConfig& config)
+{
+    if (config.textures)
+        set_state_textures(*config.textures);
+    else
+        clear_state_textures();
+
+    if (config.sounds)
+        set_sounds(*config.sounds);
+    else
+        clear_sounds();
+
+    set_draw_background(config.draw_background);
+    set_draw_border(config.draw_border);
+}
+
+bool UiCheckbox::set_state_internal(UiCheckboxState state,bool notify) noexcept
+{
+    if (_state == state)
+        return false;
+
+    _state = state;
+    if (notify && _on_toggled)
+        _on_toggled(_state);
+    return true;
+}
+
+bool UiCheckbox::toggle_internal(bool notify,bool play_toggle_sound) noexcept
+{
+    const bool changed = set_state_internal(toggled_state(_state),notify);
+    if (changed && play_toggle_sound && _sounds)
+        play_sound_if_set(_sounds->toggle);
+    return changed;
+}
+
+bool UiCheckbox::can_interact() const noexcept
+{
+    return is_enabled() && is_focused() && is_active() && is_visible();
+}
+
+bool UiCheckbox::can_receive_pointer() const noexcept
+{
+    return is_enabled() && is_active() && is_visible();
+}
+
+bool UiCheckbox::contains_pointer(int mouse_x,int mouse_y) const noexcept
+{
+    return screen_rect().contains(elysia::core::Vector2(static_cast<float>(mouse_x),static_cast<float>(mouse_y)));
+}
+
+bool UiCheckbox::is_primary_pointer_event(const UiInputEvent& event) const noexcept
+{
+    return event.device == elysia::input::InputDevice::Mouse
+        && event.control == elysia::input::RawInputControl::MouseLeft;
+}
+
+void UiCheckbox::clear_pushed_state() noexcept
+{
+    _is_pushed = false;
+}
+
+void UiCheckbox::play_sound_if_set(const std::string& sound_key) const
+{
+    if (sound_key.empty())
+        return;
+    elysia::audio::AudioService::instance()->play_sound(sound_key);
+}
+
+elysia::core::Rect UiCheckbox::content_rect() const noexcept
+{
+    const elysia::core::Rect& control_rect = screen_rect();
+    const float width = std::max(0.0f,control_rect.width());
+    const float height = std::max(0.0f,control_rect.height());
+    const float padding = static_cast<float>(_padding);
+    const float pad_x = std::min(padding,width * 0.5f);
+    const float pad_y = std::min(padding,height * 0.5f);
+
+    elysia::core::Rect content = control_rect;
+    content.set_x(control_rect.x() + pad_x);
+    content.set_y(control_rect.y() + pad_y);
+    content.set_width(width - pad_x * 2.0f);
+    content.set_height(height - pad_y * 2.0f);
+    return content;
+}
+
+elysia::core::Rect UiCheckbox::checkbox_rect() const noexcept
+{
+    const elysia::core::Rect content = content_rect();
+    if (content.is_empty())
+        return elysia::core::Rect::zero();
+
+    const float side = std::min(content.width(),content.height());
+    if (side <= 0.0f)
+        return elysia::core::Rect::zero();
+
+    return elysia::core::Rect::from_center(content.center(),elysia::core::Vector2(side,side));
+}
+
+const UiCheckboxVisualStateTextures* UiCheckbox::current_state_textures() const noexcept
+{
+    if (!_textures)
+        return nullptr;
+
+    switch (_state)
+    {
+    case UiCheckboxState::Checked:
+        return &_textures->checked;
+    case UiCheckboxState::Indeterminate:
+        return &_textures->indeterminate;
+    case UiCheckboxState::Unchecked:
+    default:
+        return &_textures->unchecked;
+    }
+}
+
+SDL_Texture* UiCheckbox::current_state_texture() const noexcept
+{
+    const UiCheckboxVisualStateTextures* textures = current_state_textures();
+    if (!textures)
+        return nullptr;
+    if (!is_enabled())
+        return textures->disabled;
+    if (_is_pushed)
+        return textures->pushed;
+    if (is_focused())
+        return textures->focused;
+    return textures->idle;
+}
+
+bool UiCheckbox::uses_texture_rendering() const noexcept
+{
+    return has_complete_state_textures();
+}
+
+elysia::core::Color UiCheckbox::current_background_color() const noexcept
+{
+    if (!is_enabled())
+        return _disabled_background_color;
+    if (_is_pushed)
+        return _pushed_color;
+    if (is_focused())
+        return _focused_color;
+    return _idle_color;
+}
+
+elysia::core::Color UiCheckbox::current_border_color() const noexcept
+{
+    return is_enabled() ? _border_color : _disabled_border_color;
+}
+
+elysia::core::Color UiCheckbox::current_checkmark_color() const noexcept
+{
+    return is_enabled() ? _checkmark_color : _disabled_checkmark_color;
+}
+
+UiCheckboxState UiCheckbox::toggled_state(UiCheckboxState state) noexcept
+{
+    switch (state)
+    {
+    case UiCheckboxState::Checked:
+        return UiCheckboxState::Unchecked;
+    case UiCheckboxState::Indeterminate:
+        return UiCheckboxState::Checked;
+    case UiCheckboxState::Unchecked:
+    default:
+        return UiCheckboxState::Checked;
+    }
+}
+}
---
+++ engine/ui/widgets/ui_checkbox.h
@@ -0,0 +1,165 @@
+#pragma once
+
+#include "../../core/render/colors.h"
+#include "../core/ui_control.h"
+
+#include <functional>
+#include <optional>
+#include <string>
+
+struct SDL_Texture;
+
+namespace elysia::ui
+{
+enum class UiCheckboxState
+{
+    Unchecked,
+    Checked,
+    Indeterminate
+};
+
+struct UiCheckboxVisualStateTextures
+{
+    SDL_Texture* idle = nullptr;
+    SDL_Texture* focused = nullptr;
+    SDL_Texture* pushed = nullptr;
+    SDL_Texture* disabled = nullptr;
+};
+
+struct UiCheckboxTextures
+{
+    UiCheckboxVisualStateTextures unchecked{};
+    UiCheckboxVisualStateTextures checked{};
+    UiCheckboxVisualStateTextures indeterminate{};
+};
+
+struct UiCheckboxSounds
+{
+    std::string focus;
+    std::string press;
+    std::string toggle;
+};
+
+struct UiCheckboxConfig
+{
+    std::optional<UiCheckboxTextures> textures = std::nullopt;
+    std::optional<UiCheckboxSounds> sounds = std::nullopt;
+    bool draw_background = true;
+    bool draw_border = true;
+};
+
+using UiCheckboxToggledCallback = std::function<void(UiCheckboxState state)>;
+
+class UiCheckbox : public UiControl
+{
+public:
+    explicit UiCheckbox(const elysia::core::Rect& rect = elysia::core::Rect::zero(),int order = 0) noexcept;
+    UiCheckbox(const elysia::core::Vector2& position,const elysia::core::Vector2& size,int order = 0) noexcept;
+    UiCheckbox(const elysia::core::Vector2& center,const elysia::core::Vector2& size,UiFromCenterTag,int order = 0) noexcept;
+
+    UiCheckbox(const elysia::core::Rect& rect,const UiCheckboxConfig& config,int order = 0) noexcept;
+    UiCheckbox(const elysia::core::Vector2& position,const elysia::core::Vector2& size,const UiCheckboxConfig& config,int order = 0) noexcept;
+    UiCheckbox(const elysia::core::Vector2& center,const elysia::core::Vector2& size,UiFromCenterTag,const UiCheckboxConfig& config,int order = 0) noexcept;
+
+    ~UiCheckbox() override = default;
+
+    void reset() noexcept override;
+
+    void set_enabled(bool enabled);
+    void set_focused(bool focused);
+
+    bool on_ui_input_event(const UiInputEvent& event) override;
+    void submit_ui_render_commands(std::vector<elysia::core::UiRenderCommand>& out_commands) const override;
+
+    void set_checkbox_config(const UiCheckboxConfig& config);
+
+    void set_state(UiCheckboxState state) noexcept;
+    [[nodiscard]] UiCheckboxState state() const noexcept;
+    void set_checked(bool checked) noexcept;
+    [[nodiscard]] bool is_checked() const noexcept;
+    [[nodiscard]] bool is_indeterminate() const noexcept;
+    void toggle();
+
+    void set_state_textures(const UiCheckboxTextures& textures);
+    void clear_state_textures() noexcept;
+    [[nodiscard]] const std::optional<UiCheckboxTextures>& state_textures() const noexcept;
+    [[nodiscard]] bool has_complete_state_textures() const noexcept;
+
+    void set_sounds(const UiCheckboxSounds& sounds);
+    void clear_sounds() noexcept;
+    [[nodiscard]] const std::optional<UiCheckboxSounds>& sounds() const noexcept;
+
+    void set_on_toggled(UiCheckboxToggledCallback on_toggled);
+
+    void set_idle_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color idle_color() const noexcept;
+
+    void set_focused_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color focused_color() const noexcept;
+
+    void set_pushed_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color pushed_color() const noexcept;
+
+    void set_disabled_background_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color disabled_background_color() const noexcept;
+
+    void set_border_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color border_color() const noexcept;
+
+    void set_disabled_border_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color disabled_border_color() const noexcept;
+
+    void set_checkmark_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color checkmark_color() const noexcept;
+
+    void set_disabled_checkmark_color(elysia::core::Color color) noexcept;
+    [[nodiscard]] elysia::core::Color disabled_checkmark_color() const noexcept;
+
+    void set_padding(int padding) noexcept;
+    [[nodiscard]] int padding() const noexcept;
+
+    void set_draw_background(bool draw_background) noexcept;
+    [[nodiscard]] bool draws_background() const noexcept;
+
+    void set_draw_border(bool draw_border) noexcept;
+    [[nodiscard]] bool draws_border() const noexcept;
+
+private:
+    void apply_checkbox_config(const UiCheckboxConfig& config);
+    [[nodiscard]] bool set_state_internal(UiCheckboxState state,bool notify) noexcept;
+    [[nodiscard]] bool toggle_internal(bool notify,bool play_toggle_sound) noexcept;
+    [[nodiscard]] bool can_interact() const noexcept;
+    [[nodiscard]] bool can_receive_pointer() const noexcept;
+    [[nodiscard]] bool contains_pointer(int mouse_x,int mouse_y) const noexcept;
+    [[nodiscard]] bool is_primary_pointer_event(const UiInputEvent& event) const noexcept;
+    void clear_pushed_state() noexcept;
+    void play_sound_if_set(const std::string& sound_key) const;
+    [[nodiscard]] elysia::core::Rect content_rect() const noexcept;
+    [[nodiscard]] elysia::core::Rect checkbox_rect() const noexcept;
+    [[nodiscard]] const UiCheckboxVisualStateTextures* current_state_textures() const noexcept;
+    [[nodiscard]] SDL_Texture* current_state_texture() const noexcept;
+    [[nodiscard]] bool uses_texture_rendering() const noexcept;
+    [[nodiscard]] elysia::core::Color current_background_color() const noexcept;
+    [[nodiscard]] elysia::core::Color current_border_color() const noexcept;
+    [[nodiscard]] elysia::core::Color current_checkmark_color() const noexcept;
+    static UiCheckboxState toggled_state(UiCheckboxState state) noexcept;
+
+private:
+    std::optional<UiCheckboxTextures> _textures;
+    std::optional<UiCheckboxSounds> _sounds;
+    UiCheckboxToggledCallback _on_toggled;
+    UiCheckboxState _state = UiCheckboxState::Unchecked;
+    elysia::core::Color _idle_color = elysia::core::colors::cobalt_blue;
+    elysia::core::Color _focused_color = elysia::core::colors::royal_blue;
+    elysia::core::Color _pushed_color = elysia::core::colors::midnight_blue;
+    elysia::core::Color _disabled_background_color = elysia::core::colors::gray_700;
+    elysia::core::Color _border_color = elysia::core::colors::sky_blue;
+    elysia::core::Color _disabled_border_color = elysia::core::colors::gray_500;
+    elysia::core::Color _checkmark_color = elysia::core::colors::glacial_white;
+    elysia::core::Color _disabled_checkmark_color = elysia::core::colors::gray_300;
+    int _padding = 4;
+    bool _draw_background = true;
+    bool _draw_border = true;
+    bool _is_pushed = false;
+};
+}
